6fc311e06ff42fef23c0a0575be9b296920ce8dc
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-10-05 22:04:51 +0530

    pdf rendered paused, upload button first

--- src/components/PdfRenderrer.tsx
+++ src/components/PdfRenderrer.tsx
@@ -1,6 +1,14 @@
 const PdfRenderer = () => {
   //
-  return <div className="">h</div>;
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      {/* pdf options */}
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">Top Bar</div>
+      </div>
+      {/* pdf options */}
+    </div>
+  );
 };
 
 export default PdfRenderer;
