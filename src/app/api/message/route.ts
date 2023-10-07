973fa935007af12dffb0fec4ab5a559eede375dc
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-10-08 00:36:46 +0530

    checkpoint pdf indexed, open ai chat reponse stream recieved -> next to recieve.display the stream response to frontend

--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,7 +1,13 @@
 import { db } from "@/db";
+import { openai } from "@/lib/openai";
+import { pinecone } from "@/lib/pinecone";
 import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
 import { NextRequest } from "next/server";
+
+import { OpenAIStream, StreamingTextResponse } from "ai";
 
 export const POST = async (req: NextRequest) => {
   // endpoint for asking questions
@@ -35,5 +41,83 @@ export const POST = async (req: NextRequest) => {
     },
   });
 
-  //
+  // vectorise the incoming the message
+  const embeddings = new OpenAIEmbeddings({
+    openAIApiKey: process.env.OPENAI_API_KEY,
+  });
+
+  const pineconeIndex = pinecone.Index("pdfgpti");
+
+  // now search for the most relevant pdf page for this query
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex,
+    namespace: file.id,
+  });
+
+  const results = await vectorStore.similaritySearch(message, 2);
+
+  //prev messages
+
+  const prevMessages = await db.message.findMany({
+    where: {
+      fileId,
+    },
+    orderBy: {
+      creadetAt: "asc",
+    },
+    take: 2,
+  });
+
+  const formattedPrevMessages = prevMessages.map((msg) => ({
+    role: msg.isUserMessage ? ("user" as const) : ("assistant" as const),
+    content: msg.text,
+  }));
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    temperature: 0,
+    stream: true,
+    messages: [
+      {
+        role: "system",
+        content:
+          "Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format.",
+      },
+      {
+        role: "user",
+        content: `Use the following pieces of context (or previous conversaton if needed) to answer the users question in markdown format. \nIf you don't know the answer, just say that you don't know, don't try to make up an answer.
+        
+              
+        \n----------------\n
+        
+        PREVIOUS CONVERSATION:
+        ${formattedPrevMessages.map((message) => {
+          if (message.role === "user") return `User: ${message.content}\n`;
+          return `Assistant: ${message.content}\n`;
+        })}
+        
+        \n----------------\n
+        
+        CONTEXT:
+        ${results.map((r) => r.pageContent).join("\n\n")}
+        
+        USER INPUT: ${message}`,
+      },
+    ],
+  });
+
+  const stream = OpenAIStream(response, {
+    async onCompletion(completion) {
+      await db.message.create({
+        data: {
+          text: completion,
+          isUserMessage: false,
+          fileId,
+          userId,
+        },
+      });
+    },
+  });
+
+  return new StreamingTextResponse(stream);
 };
--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,6 +1,10 @@
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { PineconeStore } from "langchain/vectorstores/pinecone";
+import { pinecone } from "@/lib/pinecone";
 
 const f = createUploadthing();
 
@@ -25,6 +29,51 @@ export const ourFileRouter = {
           uploadStatus: "PROCESSING",
         },
       });
+
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        );
+
+        const blob = await response.blob();
+
+        const loader = new PDFLoader(blob);
+
+        const pageLevelDocs = await loader.load();
+
+        const pageAmt = pageLevelDocs.length;
+
+        // vectorise and index entire document
+
+        const pineconeIndex = pinecone.Index("pdfgpti");
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        });
+
+        await PineconeStore.fromDocuments(pageLevelDocs, embeddings, {
+          pineconeIndex,
+          namespace: createdFile.id,
+        });
+
+        await db.file.update({
+          data: {
+            uploadStatus: "SUCCESS",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      } catch (error) {
+        await db.file.update({
+          data: {
+            uploadStatus: "FAILED",
+          },
+          where: {
+            id: createdFile.id,
+          },
+        });
+      }
     }),
 } satisfies FileRouter;
 
