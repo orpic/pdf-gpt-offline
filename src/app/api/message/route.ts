7f605f35375764e1e967ce1988b2ff8d4fb70be6
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-10-07 22:15:55 +0530

    checkpoint context creation half way -> ai semantic query, indexing the full pdf part

---
+++ src/app/api/message/route.ts
@@ -0,0 +1,39 @@
+import { db } from "@/db";
+import { SendMessageValidator } from "@/lib/validators/SendMessageValidator";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { NextRequest } from "next/server";
+
+export const POST = async (req: NextRequest) => {
+  // endpoint for asking questions
+
+  const body = await req.json();
+
+  const { getUser } = getKindeServerSession();
+  const user = getUser();
+
+  const { id: userId } = user;
+
+  if (!userId) return new Response("Unauthorised", { status: 401 });
+
+  const { fileId, message } = SendMessageValidator.parse(body);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileId,
+      userId,
+    },
+  });
+
+  if (!file) return new Response("Not found", { status: 404 });
+
+  await db.message.create({
+    data: {
+      text: message,
+      isUserMessage: true,
+      userId,
+      fileId,
+    },
+  });
+
+  //
+};
