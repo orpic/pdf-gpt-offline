7e06cf707bb0641395516b462db370d195d647c0
Shobhit Umrao <shobhitumrao5@gmail.com>
2023-10-10 11:32:50 +0530

    checkpoint - infinite message loading complete -> next navbar, paywall stripe

--- src/components/chat/Message.tsx
+++ src/components/chat/Message.tsx
@@ -1,6 +1,6 @@
 import { cn } from "@/lib/utils";
 import { ExtendedMessage } from "@/types/message";
-import { FC } from "react";
+import { FC, forwardRef } from "react";
 import { Icons } from "../Icons";
 import ReactMarkdown from "react-markdown";
 import { format } from "date-fns";
@@ -10,73 +10,76 @@ interface MessageProps {
   isNextmessageSamePerson: boolean;
 }
 
-const Message: FC<MessageProps> = ({ message, isNextmessageSamePerson }) => {
-  return (
-    <div
-      className={cn("flex items-end", {
-        "justify-end": message.isUserMessage,
-      })}
-    >
+// eslint-disable-next-line react/display-name
+const Message = forwardRef<HTMLDivElement, MessageProps>(
+  ({ message, isNextmessageSamePerson }, ref) => {
+    return (
       <div
-        className={cn(
-          "relative flex h-6 w-6 aspect-square items-center justify-center",
-          {
-            "order-2 bg-blue-600 rounded-sm": message.isUserMessage,
-            "order-1 bg-zinc-800 rounded-sm ": !message.isUserMessage,
-            invisible: isNextmessageSamePerson,
-          }
-        )}
-      >
-        {message.isUserMessage ? (
-          <Icons.user className="fill-zinc-200 text-zinc-200 h-3/4 w-3/4" />
-        ) : (
-          <Icons.logo className="fill-zinc-300 h-3/4 w-3/4" />
-        )}
-      </div>
-
-      {/*  */}
-      <div
-        className={cn("flex flex-col space-y-2 text-base max-w-md mx-2", {
-          "order-1 items-end": message.isUserMessage,
-          "order-2 items-start": !message.isUserMessage,
+        ref={ref}
+        className={cn("flex items-end", {
+          "justify-end": message.isUserMessage,
         })}
       >
         <div
-          className={cn("px-4 py-2 rounded-lg inline-block", {
-            "bg-blue-600 text-white": message.isUserMessage,
-            "bg-gray-200 text-gray-900": !message.isUserMessage,
-            "rounded-br-none":
-              !isNextmessageSamePerson && message.isUserMessage,
-            "rounded-bl-none":
-              !isNextmessageSamePerson && !message.isUserMessage,
+          className={cn(
+            "relative flex h-6 w-6 aspect-square items-center justify-center",
+            {
+              "order-2 bg-blue-600 rounded-sm": message.isUserMessage,
+              "order-1 bg-zinc-800 rounded-sm ": !message.isUserMessage,
+              invisible: isNextmessageSamePerson,
+            }
+          )}
+        >
+          {message.isUserMessage ? (
+            <Icons.user className="fill-zinc-200 text-zinc-200 h-3/4 w-3/4" />
+          ) : (
+            <Icons.logo className="fill-zinc-300 h-3/4 w-3/4" />
+          )}
+        </div>
+
+        {/*  */}
+        <div
+          className={cn("flex flex-col space-y-2 text-base max-w-md mx-2", {
+            "order-1 items-end": message.isUserMessage,
+            "order-2 items-start": !message.isUserMessage,
           })}
         >
-          {typeof message.text === "string" ? (
-            <ReactMarkdown
-              className={cn("prose", {
-                "text-zinc-50": message.isUserMessage,
-              })}
-            >
-              {message.text}
-            </ReactMarkdown>
-          ) : (
-            message.text
-          )}
-          {/*  */}
-          {message.id !== "loading-message" ? (
-            <div
-              className={cn("text-xs select-none mt-2 w-full text-right", {
-                "text-zinc-500": !message.isUserMessage,
-                "text-blue-300": message.isUserMessage,
-              })}
-            >
-              {format(new Date(message.creadetAt), "HH:mm")}
-            </div>
-          ) : null}
+          <div
+            className={cn("px-4 py-2 rounded-lg inline-block", {
+              "bg-blue-600 text-white": message.isUserMessage,
+              "bg-gray-200 text-gray-900": !message.isUserMessage,
+              "rounded-br-none":
+                !isNextmessageSamePerson && message.isUserMessage,
+              "rounded-bl-none":
+                !isNextmessageSamePerson && !message.isUserMessage,
+            })}
+          >
+            {typeof message.text === "string" ? (
+              <ReactMarkdown
+                className={cn("prose", {
+                  "text-zinc-50": message.isUserMessage,
+                })}
+              >
+                {message.text}
+              </ReactMarkdown>
+            ) : (
+              message.text
+            )}
+            {/*  */}
+            {message.id !== "loading-message" ? (
+              <div
+                className={cn("text-xs select-none mt-2 w-full text-right", {
+                  "text-zinc-500": !message.isUserMessage,
+                  "text-blue-300": message.isUserMessage,
+                })}
+              >
+                {format(new Date(message.creadetAt), "HH:mm")}
+              </div>
+            ) : null}
+          </div>
         </div>
       </div>
-    </div>
-  );
-};
-
+    );
+  }
+);
 export default Message;
--- src/components/chat/Messages.tsx
+++ src/components/chat/Messages.tsx
@@ -1,10 +1,11 @@
 import { trpc } from "@/app/_trpc/client";
 import { INFINITE_QUERY_LIMIT } from "@/config/infinite-query";
 import { Loader2, MessageSquare } from "lucide-react";
-import { FC, useContext } from "react";
+import { FC, useContext, useEffect, useRef } from "react";
 import Skeleton from "react-loading-skeleton";
 import Message from "./Message";
 import { ChatContext } from "./ChatContext";
+import { useIntersection } from "@mantine/hooks";
 
 interface MessagesProps {
   fileId: string;
@@ -43,6 +44,19 @@ const Messages: FC<MessagesProps> = ({ fileId }) => {
     ...(messages ?? []),
   ];
 
+  const lastMessageRef = useRef<HTMLDivElement>(null);
+
+  const { ref, entry } = useIntersection({
+    root: lastMessageRef.current,
+    threshold: 1,
+  });
+
+  useEffect(() => {
+    if (entry?.isIntersecting) {
+      fetchNextPage();
+    }
+  }, [entry, fetchNextPage]);
+
   return (
     <div className="flex max-h-[calc(100vh-3.5rem-7rem)] border-zinc-200 flex-1 flex-col-reverse gap-4 p-3 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch">
       {combinedMessages && combinedMessages.length > 0 ? (
@@ -54,6 +68,7 @@ const Messages: FC<MessagesProps> = ({ fileId }) => {
           if (i === combinedMessages.length - 1) {
             return (
               <Message
+                ref={ref}
                 message={message}
                 key={message.id}
                 isNextmessageSamePerson={isNextMessageSamePerson}
